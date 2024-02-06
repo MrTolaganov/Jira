43062c0f95085871eea41cc3c22115112db4701e
Otabek <otabektolaganov69@gmail.com>
2024-02-06 11:49:50 +0500

    06/02/2024 11:49

---
+++ pages/auth.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({ layout: "auth" });
+</script>
+
+<template>
+  <div>
+    <h1>Auth</h1>
+    <SharedButton>Button</SharedButton>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useHead({ title: "Jira software" });
+</script>
+
+<template>
+  <div>
+    <h1>Home page</h1>
+  </div>
+</template>
